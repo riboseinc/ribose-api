a1c122bb9106d5cf6c7bd1f43671fe9537de0724
Sebastian Skałacki <skalee@gmail.com>
2017-11-15 23:49:34 +0100

    Resolve transclusions to fixtures in ribose-ruby

--- bin/build.js
+++ bin/build.js
@@ -7,13 +7,20 @@
 
 "use strict"
 
+let findRoot = require("find-root")
+let fs = require("fs")
 let hercule = require("hercule")
+let path = require("path")
 
 let resolvers = [
+  importFixture,
   hercule.resolveHttpUrl,
   hercule.resolveLocalUrl,
   hercule.resolveString,
 ]
+
+let projectRoot = findRoot(require.main.filename)
+let fixturesRoot = path.join(projectRoot, "fixtures", "spec", "fixtures")
 
 let input = process.stdin
 let output = process.stdout
@@ -26,3 +33,19 @@ transcluder.on("error", (err) => {
 })
 
 input.pipe(transcluder).pipe(output)
+
+/// Resolves response fixture links like fixture:spaces.json
+///
+/// Heavily based on:
+/// https://github.com/jamesramsay/hercule/blob/24b14ca/src/resolver.js#L24-L34
+function importFixture(url, source) {
+  let match = url.match("^fixture:(.*)$")
+
+  if (match) {
+    let fixturePath = path.join(fixturesRoot, match[1])
+    let fixture = fs.createReadStream(fixturePath, {encoding: "utf8"})
+    return {content: fixture, url: fixturePath}
+  } else {
+    return null
+  }
+}
